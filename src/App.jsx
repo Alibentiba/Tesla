55451bebf8caa38c2d755e7c1e1e331b88ea9189
Alibentiba <99966047+Alibentiba@users.noreply.github.com>
2022-06-25 02:38:33 +0100

    heder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 // import Navbar from "../src/Navbar/Navbar.jsx"
 import React from "react"
 import "./App.css";
+import Header from "./Header/Header";
 import Home from "./Home/Home.jsx";
 function App() {
   return (
     <div className="App">
+      <Header/>
     <Home/>
     </div>
   );
---
+++ src/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./Header.css"
+const Header = () => {
+  return (
+    <div className='Header'>
+        <img src="images/logo.svg" alt="logo" />
+<div className='menu'>
+    <a href="">Model S</a>
+    <a href="">Model 3</a>
+    <a href="">Model X</a>
+    <a href="">Model Y</a>
+    <a href="">Solar Roof</a>
+    <a href="">Solar Panels</a>
+
+</div>
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Header
